3d2070b6d3b8fda3c58021bcd1bef1b9131ce928
neeschit <nischit14@gmail.com>
2018-02-23 23:15:44 -0500

    add service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule.withServerTransition({ appId: 'mbta-universal' })],
+    imports: [
+        BrowserModule.withServerTransition({ appId: 'mbta-universal' }),
+        ServiceWorkerModule.register('/ngsw-worker.js', {
+            enabled: environment.production
+        })
+    ],
     providers: [],
     bootstrap: [AppComponent]
 })
