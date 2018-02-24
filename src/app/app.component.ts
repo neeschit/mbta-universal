449b04bc53a9ffe42160622996cda3d4403ef7fa
neeschit <nischit14@gmail.com>
2018-02-24 00:32:50 -0500

    add meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-root',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'Realtime MBTA tracker for the greater Boston area bus, subway and commuter rail';
+
+    constructor(private meta: Meta) {
+        this.meta.addTag({
+            name: 'description',
+            content: 'Realtime MBTA tracker for the greater Boston area bus, subway and commuter rail'
+        });
+        this.meta.addTag({ name: 'author', content: 'nischit.burnz@gmail.com' });
+        this.meta.addTag({
+            name: 'keywords',
+            content: 'MBTA, Tracker, Boston, Bus, Subway, Commuter, Rail'
+        });
+    }
 }
